15b0678025cb84ece60d1b82874a3cf7c74b2993
U.A. Yanık <atesyanik@gmail.com>
2021-10-13 23:40:59 +0200

    material ui added

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
-        collectionTypes: [`variant`,`category`,`product`, `user`],
+        collectionTypes: [`variant`,`category`,`product`],
       },
     },
     {
