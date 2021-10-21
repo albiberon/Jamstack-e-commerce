e6ec3793c5c66d15a2894fe330c5fb752dd467b4
U.A. Yanık <atesyanik@gmail.com>
2021-10-21 23:38:17 +0200

    first graphql query

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography, Button, Tabs, Tab, IconButton} from '@material-ui/core'
 
-
 import search from '../../images/search.svg'
 import cart from '../../images/cart.svg'
 import account from '../../images/account.svg'
+import headerStyles from './headerStyles'
 
-const Header = () => {
+
+const Header = ({categories}) => {
+  // console.log("Header", categories)
+  const classes = headerStyles()
+  const routes = [
+    ...categories, 
+    {node: {name: 'Contact Us', strapiId: 'contact'}}
+  ]
+
   return (
       <AppBar color="transparent" elevation={0}>
         <Toolbar>
           <Button>
             <Typography variant="h1">
-                Codaman
+                <span className={classes.logoText}>Coda</span>man
             </Typography>
-            <Tabs>
-              <Tab label="Hats"/>
-              <Tab label="Shirts"/>
-              <Tab label="Hoodies"/>
-              <Tab label="Contact us"/>
+            <Tabs value={0} classes={{ indicator: classes.coloredIndicator, root: classes.tabs}}>
+              {
+                routes.map(route => (
+                <Tab label={route.node.name} key={route.node.strapiId}/>
+                ))
+              }
+
             </Tabs>
             <IconButton>
               <img width="25" src={search} alt="search"/>
